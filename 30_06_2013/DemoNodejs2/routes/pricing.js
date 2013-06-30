5b9421e78f02070ccfb0a2628b678a31d6a2a5fa
Zeeshan Hanif <zeeshanhanif@gmail.com>
2013-07-01 03:52:28 +0500

    Class work 30 June 2013

---
+++ 30_06_2013/DemoNodejs2/routes/pricing.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zeeshanhanif
+ * Date: 6/30/13
+ * Time: 2:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.priceList = function (req,res){
+    res.render("pricing");
+}
