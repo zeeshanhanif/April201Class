d6f357ca8cac1129fb7d3641d197b34389daaa52
Zeeshan Hanif <zeeshanhanif@gmail.com>
2013-07-11 20:14:09 +0500

    Class work 7 July 2013

---
+++ 07_07_2013/DemoNodejs1/routes/login.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zeeshanhanif
+ * Date: 7/7/13
+ * Time: 2:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.loginSuccess = function(req,res){
+    ////
+    ////
+
+    res.render('calculator');
+
+}
