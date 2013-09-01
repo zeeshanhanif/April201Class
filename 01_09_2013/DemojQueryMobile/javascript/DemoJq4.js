e8bad029f58405712ed015bf1581384450915a26
Zeeshan Hanif <zeeshanhanif@gmail.com>
2013-09-01 14:57:27 +0500

    class work 1 September 203

---
+++ 01_09_2013/DemojQueryMobile/javascript/DemoJq4.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zeeshanhanif
+ * Date: 9/1/13
+ * Time: 2:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function (){
+   //alert("testing");
+    //$("#radio-italian").attr("checked", false).checkboxradio("refresh");
+    //$("#radio-greek").attr("checked", true).checkboxradio("refresh");
+});
---
+++ 01_09_2013/DemojQueryMobile/javascript/myscript.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zeeshanhanif
+ * Date: 8/25/13
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(function (){
+
+    $("#page2").bind("pageshow",function (event,ui){
+       alert("Hello world");
+    });
+
+})
